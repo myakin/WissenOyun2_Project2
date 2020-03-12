89d4bf428d924de83010348bff98bd24ec46baba
Murat Yakin <yakin_murat@yahoo.com>
2020-03-12 21:39:16 +0300

    Karakter hareketleri ve sonsuz akis yapildi

---
+++ Assets/0-Scripts/EndlessMovementManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessMovementManager : MonoBehaviour {
+    public Transform groundSet1, groundSet2;
+    public Transform spawnPointDummy, movePointDummy;
+    public float moveSpeed = 0.1f;
+
+    private void Update() {
+        groundSet1.transform.position = new Vector3(
+            groundSet1.transform.position.x-moveSpeed,
+            0,
+            0);
+        groundSet2.transform.position = new Vector3(
+            groundSet2.transform.position.x-moveSpeed,
+            0,
+            0);
+
+        if (groundSet1.transform.position.x < movePointDummy.position.x) {
+            groundSet1.transform.position = spawnPointDummy.position;
+        }
+        if (groundSet2.transform.position.x < movePointDummy.position.x) {
+            groundSet2.transform.position = spawnPointDummy.position;
+        }
+    }
+}
---
+++ Assets/0-Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float forceMagnitude = 100f;
+
+    
+    private void Update() {
+        
+        if (Input.GetMouseButtonDown(0)) {
+            ApplyAntiGravityForce();
+        }
+
+        //Touch tou = Input.GetTouch(0);
+        //if (tou.phase == TouchPhase.Began) {
+        //    ApplyAntiGravityForce();
+        //}
+
+        
+    }
+
+    private void ApplyAntiGravityForce() {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * forceMagnitude, ForceMode2D.Impulse);
+    }
+}
