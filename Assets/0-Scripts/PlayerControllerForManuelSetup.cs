cc42f9002a33ad950b4a25625b8173ae015aaeae
Murat Yakin <yakin_murat@yahoo.com>
2020-04-09 18:40:17 +0300

    Manuel setup 1

---
+++ Assets/0-Scripts/PlayerControllerForManuelSetup.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerForManuelSetup : MonoBehaviour {
+    public float forceMagnitude = 100f;
+    public float rightMovementMagnitude = 5f;
+    public bool isDead;
+    // public Sprite deadSprite;
+    // public Sprite liveSprite;
+    // public SpriteRenderer playerSpriteRenderer;
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+
+    private void Start() {
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+        InitiatePlayer();
+    }
+
+    public void InitiatePlayer() {
+        transform.position = initialPosition;
+        transform.rotation = initialRotation;
+        GetComponent<Animator>().SetBool("isDead", false);
+        // playerSpriteRenderer.sprite = liveSprite;
+        GetComponent<Rigidbody2D>().AddForce(Vector2.right * rightMovementMagnitude, ForceMode2D.Impulse);
+        isDead = false;
+    }
+    
+    private void Update() {
+        if (!isDead && Input.GetMouseButtonDown(0)) {
+            ApplyAntiGravityForce();
+        }
+
+        //Touch tou = Input.GetTouch(0);
+        //if (tou.phase == TouchPhase.Began) {
+        //    ApplyAntiGravityForce();
+        //}  
+    }
+
+    private void ApplyAntiGravityForce() {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * forceMagnitude, ForceMode2D.Impulse);
+    }
+
+    
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        Die();
+    }
+
+    private void Die() {
+        GetComponent<Animator>().SetBool("isDead", true);
+        // playerSpriteRenderer.sprite = deadSprite;
+        isDead = true;
+        GameObject.FindGameObjectWithTag("UIInterface").GetComponent<UIManager>().ActivteMainMenu();
+    }
+}
---
+++ Assets/0-Scripts/UIManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    public Transform backgroundPanel, mainMenuPanel;
+    public Button newGameButton, hiScoresButton, exitGameButton;
+
+    private void Start() {
+        backgroundPanel.gameObject.SetActive(false);
+        mainMenuPanel.gameObject.SetActive(false);
+        newGameButton.onClick.AddListener(StartNewGame);
+        hiScoresButton.onClick.AddListener(GetHiScores);
+        exitGameButton.onClick.AddListener(QuitGame);
+    }
+
+    public void ActivteMainMenu() {
+        backgroundPanel.gameObject.SetActive(true);
+        mainMenuPanel.gameObject.SetActive(true);
+    }
+
+    private void StartNewGame() {
+        backgroundPanel.gameObject.SetActive(false);
+        mainMenuPanel.gameObject.SetActive(false);
+        GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<PlayerControllerForManuelSetup>().InitiatePlayer();
+    }
+
+    private void GetHiScores() {
+
+    }
+
+    private void QuitGame() {
+
+    }
+
+
+
+}
